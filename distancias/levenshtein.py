6d7b33fa1ae149ded642b5842322cbfab2a2eca1
Lanver123 <menoxzhan@gmail.com>
2019-10-07 11:54:38 +0200

    Pintate una Cervantes

--- distancias/levenshtein.py
+++ distancias/levenshtein.py
@@ -6,12 +6,6 @@
 # Martínez Bernia, Javier
 # Murcia Serrano, Andrea
 
-######      Indexer      ######
-# Extrae las noticias de una 
-# colección de documentos alojados 
-# en un directorio, las indexa y 
-# guarda en disco los índices creados
-
 import sys
 import os
 import pprint
@@ -20,120 +14,45 @@ import re
 import pickle
 
 def syntax():
-    print("argumentos <coleccion dir>")
+    print("argumentos <archivo.txt> <palabra> <distancia max>")
     exit(1)
 
-def indexarCuerpo(directorioInicio):
-    """ Devuelve una tupla con (IndiceInvertido, DiccionarioDocumentos, idsNoticias, IndicesStemming) """
+def levenshtein_distance(x,y):
+    D = {}
+    D[0,0] = 0
+    for i in range(1,len(x)+1):
+        D[i,0] = D[i-1, 0] + 1
+    for j in range(1, len(y)+1):
+        D[0,j] = D[0, j-1] + 1
+        for i in range(1, len(x)+1):
+            D[i,j] = min(D[i-1,j]+1, D[i,j-1]+1,D[i-1,j-1]+(x[i-1] != y[j-1]))
+    return D[len(x),len(y)]
 
-    indiceInvertidoArticle= {}
-    indiceInvertidoTitle= {}
-    indiceInvertidoSummary= {}
-    indiceInvertidoKeywords= {}
-    indiceInvertidoDate= {}
-    indices = {}
-    indicesStemming = {}
-    indiceStemmArticle={}
-    indiceStemmTitle={}
-    indiceStemmSummary={}
-    indiceStemmKeywords={}
-    indiceStemmDate={}
-    diccionarioDocumentos={}
-    
-    stemmer = SnowballStemmer('spanish')
-    idsNoticias=[]
-    nNoticias = 0
-    numeroDocumento = 0
-    for dirName, _, fileList in os.walk(directorioInicio):
-        for fname in fileList:
-            numeroNoticia = 0
-            rel_file = os.path.join(dirName, fname)
-            with open(rel_file, 'r') as json_file:  
-                data = json.load(json_file)
-            for i in range(len(data)):
-                nNoticias += 1
-                idNoticia = (numeroDocumento,numeroNoticia)
-                idsNoticias.append(idNoticia)
-                diccionarioDocumentos[idNoticia]=(rel_file,numeroNoticia)
-                numeroNoticia = numeroNoticia+1
-                er = re.compile(r'\w+')
-                for word in er.findall(str(data[i]['article'])):
-
-                    indiceInvertidoArticle.setdefault(word.lower(), [])
-                    indiceInvertidoArticle[word.lower()] = list(set().union(indiceInvertidoArticle[word.lower()], [idNoticia]))
-
-                    stemWord = stemmer.stem(word.lower())
-                    indiceStemmArticle.setdefault(stemWord,[])
-                    indiceStemmArticle[stemWord] = list(set().union(indiceStemmArticle[stemWord],[idNoticia]))
-
-                for word in er.findall(str(data[i]['title'])):
-
-                    indiceInvertidoTitle.setdefault(word.lower(), [])
-                    indiceInvertidoTitle[word.lower()] = list(set().union(indiceInvertidoTitle[word.lower()], [idNoticia]))  
-
-                    stemWord = stemmer.stem(word.lower())
-                    indiceStemmTitle.setdefault(stemWord,[])
-                    indiceStemmTitle[stemWord] = list(set().union(indiceStemmTitle[stemWord],[idNoticia]))
-                for word in er.findall(str(data[i]['summary'])):
-
-                    indiceInvertidoSummary.setdefault(word.lower(), [])
-                    indiceInvertidoSummary[word.lower()] = list(set().union(indiceInvertidoSummary[word.lower()], [idNoticia]))  
-
-                    stemWord = stemmer.stem(word.lower())
-                    indiceStemmSummary.setdefault(stemWord,[])
-                    indiceStemmSummary[stemWord] = list(set().union(indiceStemmSummary[stemWord],[idNoticia]))                      
-                for word in er.findall(str(data[i]['keywords'])):
-
-                    indiceInvertidoKeywords.setdefault(word.lower(), [])
-                    indiceInvertidoKeywords[word.lower()] = list(set().union(indiceInvertidoKeywords[word.lower()], [idNoticia]))  
-
-                    stemWord = stemmer.stem(word.lower())
-                    indiceStemmKeywords.setdefault(stemWord,[])
-                    indiceStemmKeywords[stemWord] = list(set().union(indiceStemmKeywords[stemWord],[idNoticia]))                
-                for word in data[i]['date'].split():
-
-                    indiceInvertidoDate.setdefault(word.lower(), [])
-                    indiceInvertidoDate[word.lower()] = list(set().union(indiceInvertidoDate[word.lower()], [idNoticia]))
-
-                    indiceStemmTitle.setdefault(word,[])
-                    indiceStemmTitle[word] = list(set().union(indiceStemmTitle[word],[idNoticia]))                                                                                                  
-            numeroDocumento = numeroDocumento+1
-
-    indices["article"] = indiceInvertidoArticle
-    indices["title"] = indiceInvertidoTitle
-    indices["summary"] = indiceInvertidoSummary
-    indices["keywords"] = indiceInvertidoKeywords
-    indices["date"] = indiceInvertidoDate
-
-    indicesStemming["article"] = indiceStemmArticle
-    indicesStemming["title"] = indiceStemmTitle
-    indicesStemming["summary"] = indiceStemmSummary
-    indicesStemming["keywords"] = indiceStemmKeywords
-    indicesStemming["date"] = indiceStemmDate
-
-    return (indices, diccionarioDocumentos,idsNoticias, indicesStemming)
-             
-
+def cercanos_levenshtein(word, diccionario, distance):
+    cercanas = []
+    for palabra in diccionario:
+        dist = levenshtein_distance(word, palabra)
+        if dist <= distance:
+            cercanas.append((palabra, dist))
+    return cercanas            
 
 if __name__ == "__main__":
     directorioColeccion = ""
-    ficheroIndice = ""
-    if len(sys.argv) != 2:
+    fichero = ""
+    if len(sys.argv) != 4:
         syntax()
 
-    ficheroIndice = sys.argv[1]
-    readFile = open(ficheroIndice, "r")
+    fichero = sys.argv[1]
+    palabra = sys.argv[2]
+    distancia = int(sys.argv[3])
+    readFile = open(fichero, "r")
 
     texto = readFile.read()
     er = re.compile("\w+")
     lines = []
+    diccionarioPalabras = {}
     for word in er.findall(texto):                
-        lines.append(word.lower())
-    print(lines)
-
-#    (indices, diccionarioDocumentos,noticias, indicesStemming) = indexarCuerpo(directorioColeccion)
-    #for subIndex in indices.values():
-    #    pprint.pprint(subIndex)
-
-
-#    pickle.dump((indices,diccionarioDocumentos,noticias,indicesStemming),open(ficheroIndice, "wb"))
+        diccionarioPalabras[word] = diccionarioPalabras.get(word, 0) + 1
+    
+    cercanos = cercanos_levenshtein(palabra, diccionarioPalabras.keys(), distancia)
+    print(cercanos)
