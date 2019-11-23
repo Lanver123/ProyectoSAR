a916460fe8a6187eb21d7397659a43ee0ee36d05
Lanver123 <menoxzhan@gmail.com>
2019-11-23 15:31:57 +0100

    Generar trie con pickle

--- distancias/damerau_branch.py
+++ distancias/damerau_branch.py
@@ -15,35 +15,8 @@ import pickle
 
 
 def syntax():
-    print("argumentos <archivo.txt> <palabra> <distancia max>")
+    print("argumentos <trieGenerado.txt> <palabra> <distancia max>")
     exit(1)
-
-def generarTrie(texto):
-    er = re.compile("\w+")
-    #diccionario que guarda todos los nodos
-    trie = {}
-    numNodes = 0
-    #cada elemento del vacabulario es una lista de nodo padre, si es palabra final y cual, y un diccionario
-    #   con la key la siguiente letra y devuelve el nodo hijo
-    trie[0] = [None, None, {}]
-    for word in er.findall(texto):
-        nodeCurrent = 0
-        for i, letter in enumerate(word):
-            #se recupera el nodo siguiente
-            nodeChild = trie[nodeCurrent][2].get(letter)
-            if nodeChild is None:
-                #si no existe el nodo siguiente, se crea, con su tripleta viendo si es palabra final y 
-                #se añade este nuevo nodo al diccionario del padre, ademas del diccionario de todos los nodos
-                numNodes = numNodes + 1
-                nodeChild = numNodes
-                childList = [nodeCurrent, None, {}]
-                if i + 1 == len(word):
-                    childList[1] = word
-                trie[nodeCurrent][2][letter] = nodeChild
-                trie[nodeChild] = childList
-            #para ir recorriendo el trie
-            nodeCurrent = nodeChild
-    return trie
 
 def calculaDistancia(trie,palabra,distancia):
     pila = [(0,0,0)]
@@ -74,9 +47,10 @@ if __name__ == "__main__":
     fichero = sys.argv[1]
     palabra = sys.argv[2]
     distancia = int(sys.argv[3])
-    readFile = open(fichero, "r")
 
-    texto = readFile.read()
-    trie = generarTrie(texto)
+    trie = {}
+    with open(fichero, 'rb') as handle:
+        trie = pickle.load(handle)
+
     cercanos = calculaDistancia(trie,palabra,distancia)
     print(cercanos)
---
+++ distancias/generarTrie.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Cardona Lorenzo, Victor
+# Gavilán Gil, Marc
+# Martínez Bernia, Javier
+# Murcia Serrano, Andrea
+
+import sys
+import os
+import pprint
+import json
+import re
+import pickle
+
+def syntax():
+    print("argumentos <archivo.txt>")
+    exit(1)
+
+def generarTrie(texto):
+    er = re.compile("\w+")
+    #diccionario que guarda todos los nodos
+    trie = {}
+    numNodes = 0
+    #cada elemento del vacabulario es una lista de nodo padre, si es palabra final y cual, y un diccionario
+    #   con la key la siguiente letra y devuelve el nodo hijo
+    trie[0] = [None, None, {}]
+    for word in er.findall(texto):
+        nodeCurrent = 0
+        for i, letter in enumerate(word):
+            #se recupera el nodo siguiente
+            nodeChild = trie[nodeCurrent][2].get(letter)
+            if nodeChild is None:
+                #si no existe el nodo siguiente, se crea, con su tripleta viendo si es palabra final y 
+                #se añade este nuevo nodo al diccionario del padre, ademas del diccionario de todos los nodos
+                numNodes = numNodes + 1
+                nodeChild = numNodes
+                childList = [nodeCurrent, None, {}]
+                if i + 1 == len(word):
+                    childList[1] = word
+                trie[nodeCurrent][2][letter] = nodeChild
+                trie[nodeChild] = childList
+            #para ir recorriendo el trie
+            nodeCurrent = nodeChild
+        
+    #Guardar el trie generado en memoria secundaria
+    with open('trieGenerado', 'wb') as handle:
+        pickle.dump(trie, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        syntax()
+    
+    readFile = open(sys.argv[1], "r")
+    texto = readFile.read()
+    
+    generarTrie(texto)
+    print("Trie generado")
