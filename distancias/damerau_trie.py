4fa1f00e29e52c823f80712071942d9332012472
CARDONA LORENZO <viccarlo@EVIRL-033-OK.dsic.cloud>
2019-12-22 17:26:57 +0100

    Afegit damerau-trie amb error

--- distancias/damerau_trie.py
+++ distancias/damerau_trie.py
@@ -16,7 +16,7 @@ import numpy as np
 
 
 def syntax():
-    print("argumentos <archivo.txt> <palabra> <distancia max>")
+    print("argumentos <trieGenerado.txt> <palabra> <distancia max>")
     exit(1)
 
 def calculaDistancia(trie,palabra,distancia):
@@ -26,7 +26,7 @@ def calculaDistancia(trie,palabra,distancia):
     for j in range(len(trie)):
         profun = 0
         padre = trie[j][0]
-        while padre != 0:
+        while padre != None:
             padre = trie[padre][0]
             profun += 1
         M[0,j] = profun
@@ -38,13 +38,20 @@ def calculaDistancia(trie,palabra,distancia):
             costeBorr = M[i-1,j]+1
             padre = trie[j][0]
             costeIns = M[i,padre] + 1
-            costeSus = M[i-1,padre] + (trie[padre][2][palabra[i-1]] != j)
-            M[i,j] = min(costeBorr,costeIns,costeSus)
-    
+            costeSus = M[i-1,padre] + (trie[padre][2].get(palabra[i-1], -1) != j)
+            costeDam = sys.maxsize
+            if i > 1 :
+                abuelo = trie[padre][0]
+                if abuelo != None:
+                    costeDam = M[i-1,padre] + (not(trie[padre][2].get(palabra[i-2], -1) == j and 
+                    trie[abuelo][2].get(palabra[i-1], -1) == padre))
+
+            M[i,j] = min(costeBorr,costeIns,costeSus,costeDam)
+            
     #Matriz llena, sacar las palabras cercanas
     palabras_cercanas = []
     for j in range (1,len(trie)):
-        if (trie[j][1]!=None and M[len(palabra),j] <= distancia):
+        if (trie[j][1] != None and M[len(palabra),j] <= distancia):
             palabras_cercanas.append(trie[j][1])
     return palabras_cercanas
 
@@ -60,7 +67,6 @@ if __name__ == "__main__":
     with open(fichero, 'rb') as handle:
         trie = pickle.load(handle)
 
-    print(trie)
     cercanos = calculaDistancia(trie,palabra,distancia)
-
-    print(cercanos)
+    print(len(cercanos), " palabras encontradas")
+    #pprint.pprint(cercanos)
